16263906b9c0d1e50a17397928c90242a50acb25
josego85 <josego85@gmail.com>
2017-08-01 00:07:08 -0400

    Se quitaron las lines grises de la x e y del grafico de actividades.

--- src/application/views/graficos.php
+++ src/application/views/graficos.php
@@ -307,17 +307,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                             'rgba(210, 99, 132, 0.6)',
                             'rgba(240, 99, 132, 0.6)'
                         ],
-                        borderColor: [
-                            'rgba(0, 99, 132, 1)',
-                            'rgba(30, 99, 132, 1)',
-                            'rgba(60, 99, 132, 1)',
-                            'rgba(90, 99, 132, 1)',
-                            'rgba(120, 99, 132, 1)',
-                            'rgba(150, 99, 132, 1)',
-                            'rgba(180, 99, 132, 1)',
-                            'rgba(210, 99, 132, 1)',
-                            'rgba(240, 99, 132, 1)'
-                        ],
                         borderWidth: 0,
                         hoverBorderWidth: 0
                     };
@@ -335,9 +324,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 fontSize: 20
                             },
                             scales: {
+                                xAxes: [{
+                                    gridLines: {
+                                        display: false
+                                    }
+                                }],
                                 yAxes: [{
-                                    ticks: {
-                                        beginAtZero: true
+                                    gridLines: {
+                                        display: false
                                     }
                                 }]
                             }
